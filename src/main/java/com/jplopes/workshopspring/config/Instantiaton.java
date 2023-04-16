44964342f8e2f7a76b4da8519c807aa7fea21468
jpllopes96 <joaolacaia96@gmail.com>
2023-04-16 19:15:59 -0300

    Reference User posts

--- src/main/java/com/jplopes/workshopspring/config/Instantiaton.java
+++ src/main/java/com/jplopes/workshopspring/config/Instantiaton.java
@@ -38,5 +38,9 @@ public class Instantiaton implements CommandLineRunner {
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+
+        userRepository.save(maria);
+
     }
 }
